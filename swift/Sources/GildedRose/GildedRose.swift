91b71db2eab0a413c0b6361667eba87b5f820115
Siroratt Suntronsuk <ann@odds.team>
2020-02-16 00:17:42 +0700

    change iterate by index to by temp name

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -12,51 +12,52 @@ public class GildedRose {
     }
     
     public func updateQuality() {
-        for i in 0..<items.count {
-            if (items[i].name != ItemName.agedBrie.rawValue && items[i].name != ItemName.backstagePasses.rawValue) {
-                if (items[i].quality > 0) {
-                    if (items[i].name != ItemName.sulfuras.rawValue) {
-                        items[i].quality = items[i].quality - 1
+        for item in items {
+            if ( item.name != ItemName.agedBrie.rawValue &&
+                 item.name != ItemName.backstagePasses.rawValue) {
+                if ( item.quality > 0) {
+                    if ( item.name != ItemName.sulfuras.rawValue) {
+                         item.quality =  item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                if ( item.quality < 50) {
+                     item.quality =  item.quality + 1
                     
-                    if (items[i].name == ItemName.backstagePasses.rawValue) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    if ( item.name == ItemName.backstagePasses.rawValue) {
+                        if ( item.sellIn < 11) {
+                            if ( item.quality < 50) {
+                                 item.quality =  item.quality + 1
                             }
                         }
                         
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if ( item.sellIn < 6) {
+                            if ( item.quality < 50) {
+                                 item.quality =  item.quality + 1
                             }
                         }
                     }
                 }
             }
             
-            if (items[i].name != ItemName.sulfuras.rawValue) {
-                items[i].sellIn = items[i].sellIn - 1
+            if ( item.name != ItemName.sulfuras.rawValue) {
+                 item.sellIn =  item.sellIn - 1
             }
             
-            if (items[i].sellIn < 0) {
-                if (items[i].name != ItemName.agedBrie.rawValue) {
-                    if (items[i].name != ItemName.backstagePasses.rawValue) {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != ItemName.sulfuras.rawValue) {
-                                items[i].quality = items[i].quality - 1
+            if ( item.sellIn < 0) {
+                if ( item.name != ItemName.agedBrie.rawValue) {
+                    if ( item.name != ItemName.backstagePasses.rawValue) {
+                        if ( item.quality > 0) {
+                            if ( item.name != ItemName.sulfuras.rawValue) {
+                                 item.quality =  item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                         item.quality =  item.quality -  item.quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if ( item.quality < 50) {
+                         item.quality =  item.quality + 1
                     }
                 }
             }
