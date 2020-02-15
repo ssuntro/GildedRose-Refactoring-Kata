c200f26d41dd0a1b6fc2227e3c4686320e760443
Siroratt Suntronsuk <ann@odds.team>
2020-02-16 00:15:08 +0700

    replace constant value, create ItemName enum

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -1,3 +1,9 @@
+
+public enum ItemName: String {
+    case agedBrie = "Aged Brie"
+    case backstagePasses = "Backstage passes to a TAFKAL80ETC concert"
+    case sulfuras = "Sulfuras, Hand of Ragnaros"
+}
 public class GildedRose {
     let items: [Item]
     
@@ -7,9 +13,9 @@ public class GildedRose {
     
     public func updateQuality() {
         for i in 0..<items.count {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i].name != ItemName.agedBrie.rawValue && items[i].name != ItemName.backstagePasses.rawValue) {
                 if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    if (items[i].name != ItemName.sulfuras.rawValue) {
                         items[i].quality = items[i].quality - 1
                     }
                 }
@@ -17,7 +23,7 @@ public class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
                     
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].name == ItemName.backstagePasses.rawValue) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
@@ -33,15 +39,15 @@ public class GildedRose {
                 }
             }
             
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (items[i].name != ItemName.sulfuras.rawValue) {
                 items[i].sellIn = items[i].sellIn - 1
             }
             
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].name != ItemName.agedBrie.rawValue) {
+                    if (items[i].name != ItemName.backstagePasses.rawValue) {
                         if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            if (items[i].name != ItemName.sulfuras.rawValue) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
@@ -60,10 +66,5 @@ public class GildedRose {
         return items.reduce("") { (result, item) -> String in
             return result + item.description + "\n"
         }
-//
-//        let numbers = [1, 2, 3, 4]
-//        let numberSum = numbers.reduce(0, { x, y in
-//            x + y
-//        })
     }
 }
